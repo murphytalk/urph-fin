f47173b3045d050aa4192878eaf6677145b357e4
Mu Lu <murphytalk@gmail.com>
2022-04-25 21:33:30 +0900

    tag dispatch

--- cxx/urph-fin-core/src/urph-fin-core.hxx
+++ cxx/urph-fin-core/src/urph-fin-core.hxx
@@ -33,10 +33,9 @@ private:
 };
 
 // tag dispatching - to help free_multiple_structs() select the right head/size
-struct first_member_tag {};
-struct second_member_tag{};
+struct default_member_tag {};
 
-template<typename Wrapper, typename T, typename MemberTag=first_member_tag>
+template<typename Wrapper, typename T, typename MemberTag=default_member_tag>
 void free_multiple_structs(Wrapper* data, MemberTag mt = MemberTag()){
     T* p = data->head(mt);
     for(int i = 0; i < data->size(mt); ++i, ++p){
@@ -61,25 +60,26 @@ public:
     Broker(const std::string&n, int ccy_num, cash_balance* first_ccy_balance, int active_funds_num, active_fund* first_fund);
     ~Broker();
 
-    inline CashBalance* head(first_member_tag) { return static_cast<CashBalance*>(first_cash_balance); }
-    inline int size(first_member_tag) { return num; } 
-    Iterator<CashBalance> begin() { return Iterator( head(first_member_tag()) ); }
-    Iterator<CashBalance> end() { return Iterator( head(first_member_tag()) + num ); }
+    inline CashBalance* head(default_member_tag) { return static_cast<CashBalance*>(first_cash_balance); }
+    inline int size(default_member_tag) { return num; } 
+    Iterator<CashBalance> begin() { return Iterator( head(default_member_tag()) ); }
+    Iterator<CashBalance> end() { return Iterator( head(default_member_tag()) + num ); }
 
-    inline ActiveFund* head(second_member_tag) { return static_cast<ActiveFund*>(first_active_fund); }
-    inline int size(second_member_tag) { return active_funds_num; }
-    Iterator<ActiveFund> fund_begin() { return Iterator( head(second_member_tag()) ); }
-    Iterator<ActiveFund> fund_end() { return Iterator( head(second_member_tag()) + active_funds_num); }
+    struct active_fund_tag{};
+    inline ActiveFund* head(active_fund_tag) { return static_cast<ActiveFund*>(first_active_fund); }
+    inline int size(active_fund_tag) { return active_funds_num; }
+    Iterator<ActiveFund> fund_begin() { return Iterator( head(active_fund_tag()) ); }
+    Iterator<ActiveFund> fund_end() { return Iterator( head(active_fund_tag()) + active_funds_num); }
 };
 
 class AllBrokers: public all_brokers{
 public:
     AllBrokers(int n, broker* broker);
     ~AllBrokers();
-    inline Broker* head(first_member_tag) { return static_cast<Broker*>(first_broker); }
-    inline int size(first_member_tag) { return num; } 
-    Iterator<Broker> begin() { return Iterator( head(first_member_tag()) ); }
-    Iterator<Broker> end() { return Iterator( head(first_member_tag()) + num ); }
+    inline Broker* head(default_member_tag) { return static_cast<Broker*>(first_broker); }
+    inline int size(default_member_tag) { return num; } 
+    Iterator<Broker> begin() { return Iterator( head(default_member_tag()) ); }
+    Iterator<Broker> end() { return Iterator( head(default_member_tag()) + num ); }
 };
 
 class Fund: public fund{
@@ -101,11 +101,11 @@ class FundPortfolio: public fund_portfolio{
 public:    
     FundPortfolio(int n, fund* fund);
     ~FundPortfolio();
-    inline Fund* head(first_member_tag) { return static_cast<Fund*>(first_fund); }
-    inline int size(first_member_tag) { return num; }
+    inline Fund* head(default_member_tag) { return static_cast<Fund*>(first_fund); }
+    inline int size(default_member_tag) { return num; }
 
-    Iterator<Fund> begin() { return Iterator( head(first_member_tag()) ); }
-    Iterator<Fund> end() { return Iterator( head(first_member_tag()) + num ); }
+    Iterator<Fund> begin() { return Iterator( head(default_member_tag()) ); }
+    Iterator<Fund> end() { return Iterator( head(default_member_tag()) + num ); }
 };
 
 // the ground rule is that none of the extended C++ classes should add member variables
