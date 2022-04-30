80f79cf75dc890f028ed6507c651eedfd7b36d16
Mu Lu <murphytalk@gmail.com>
2022-05-01 01:25:56 +0900

    modal popup PoC

--- electron-app/src/frontend/App.svelte
+++ electron-app/src/frontend/App.svelte
@@ -3,34 +3,43 @@
   @tailwind components;
   @tailwind utilities;
 
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+  #modal-overlay {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  background-color: black;
+  opacity: 0.5;  
+}  
 </style>
 
 <script lang="ts">
   //import Icon from "svelte-awesome";
   //import { infoCircle } from "svelte-awesome/icons";
+  let aboutVisible = false;
+
+  function showAbout(): void{
+    console.log('show about');
+    aboutVisible = true;
+  }
+
+  function closeAbout(): void{
+    aboutVisible = false;
+  }
 </script>
 
 <div>
-  <nav class="bg-white dark:bg-gray-800  shadow ">
+  <nav class="bg-white dark:bg-gray-800 shadow">
     <div class="max-w-7xl mx-auto px-2">
       <div class="flex items-center justify-between h-12">
         <div class=" flex items-center">
-          <a class="flex-shrink-0" href="/">
+          <a class="flex-shrink-0" href="#" on:click="{showAbout}">
             <img class="h-8 w-8" src="images/crab.png" alt="Logo" />
           </a>
           <div class="hidden md:block">
-            <div class="ml-10 flex items-baseline space-x-4">
+            <div class="ml-2 flex items-baseline space-x-2">
               <a
                 class="text-gray-800  dark:text-white  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 href="/#"
@@ -62,3 +71,46 @@
     </div>
   </nav>
 </div>
+
+<div hidden = {!aboutVisible} class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white z-10" 
+>
+	<div class="mt-3 text-center">
+		<div
+			class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-blue-100"
+		>
+			<svg
+				class="h-6 w-6 text-blue-600"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M5 13l4 4L19 7"
+				></path>
+			</svg>
+		</div>
+		<h3 class="text-lg leading-6 font-medium text-gray-900">Successful!</h3>
+		<div class="mt-2 px-7 py-3">
+			<p class="text-sm text-gray-500">
+				Account has been successfully registered!
+			</p>
+		</div>
+		<div class="items-center px-4 py-3">
+			<button
+				id="ok-btn"
+				class="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+        on:click="{closeAbout}"
+			>
+				OK
+			</button>
+		</div>
+	</div>
+</div>
+
+{#if aboutVisible}
+  <div id='modal-overlay'></div>
+{/if}
