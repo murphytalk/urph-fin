20c76ef9e3b81ecaf24832039a5730326c78e9a5
Mu Lu <murphytalk@gmail.com>
2022-05-01 13:35:36 +0900

    fixed dlg position

--- electron-app/src/frontend/share/ModalDlg.svelte
+++ electron-app/src/frontend/share/ModalDlg.svelte
@@ -1,5 +1,5 @@
 <style lang="postcss">
- .modal-overlay {
+  .modal-overlay {
     width: 100%;
     height: 100%;
     position: fixed;
@@ -7,30 +7,31 @@
     left: 0;
     z-index: 0;
     background-color: black;
-    opacity: 0.5;  
-}  
+    opacity: 0.5;
+  }
 </style>
 
 <script lang="ts">
   export let shown: boolean = false;
 </script>
 
-
-<div hidden = {!shown} class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white z-10">
-	<div class="mt-3 text-center">
-        <slot></slot>
-		<div class="items-center px-4 py-3">
-			<button
-				id="ok-btn"
-				class="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
-                on:click="{() => shown=false}">
-                OK
-			</button>
-		</div>
+<div hidden="{!shown}" class="fixed w-full top-20 z-10 ">
+  <div class="relative mx-auto w-96 p-5 border shadow-lg rounded-md bg-white z-10 ">
+    <div class="mt-3 text-center">
+      <slot />
+      <div class="items-center px-4 py-3">
+        <button
+          id="ok-btn"
+          class="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+          on:click="{() => (shown = false)}"
+        >
+          OK
+        </button>
+      </div>
     </div>
+  </div>
 </div>
 
 {#if shown}
   <div class="modal-overlay"></div>
 {/if}
-
