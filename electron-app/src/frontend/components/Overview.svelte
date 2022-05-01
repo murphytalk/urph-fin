e1c9d4f7afec249086b3582dd37eb838cd26c187
Mu Lu <murphytalk@gmail.com>
2022-05-02 00:08:34 +0900

    get ag grid work - tuning syles

--- electron-app/src/frontend/components/Overview.svelte
+++ electron-app/src/frontend/components/Overview.svelte
@@ -1,44 +1,52 @@
 <script lang="ts">
-import { onMount, onDestroy } from "svelte";
-import { Grid } from "ag-grid-community";
-import "ag-grid-community/dist/styles/ag-grid.css";
-import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import type { ColDef, ColGroupDef } from "ag-grid-community";
 
-let ref;
-let grid: Grid;
-let api;
+import DataGrid from "../share/DataGrid.svelte";
+import { currencySign, formatNumber } from "../utils";
 
-const columnDefs = [
-  { field: "make" },
-  { field: "model" },
-  { field: "price" }
-];    
+interface OverviewItem {
+  broker?: string;
+  asset?: string;
+  ccy?: string;
+  marketValue?: number;
+  marketValueBaseCcy: number;
+  profit?: number;
+  profitBaseCcy: number;
+}
 
-const rowData = [
-  { make: "Toyota", model: "Celica", price: 35000 },
-  { make: "Toyota", model: "Celica 2", price: 35000 },
-  { make: "Ford", model: "Mondeo", price: 32000 },
-  { make: "Porsche", model: "Boxster", price: 72000 }
-];
+// for ag-grid
+function currencyFormatter(params: any) {
+  // console.log('overview num field param %o', params);
+  const overview = params.data as OverviewItem;
+  // tslint:disable-next-line: max-line-length
+  return params.value == null ? '' : `${params.colDef.headerName === 'JPY' ? '\u00a5' : currencySign(overview.ccy)} ${formatNumber(params.value)}`;
+}
 
-// let the grid know which columns and what data to use
-const gridOptions = {
-  columnDefs: columnDefs,
-  rowData: rowData
-};
 
-onMount( () => {
-    grid = new Grid(ref, gridOptions);    
-});
+let data = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 },
+  ];
 
-onDestroy(() => {
-    if (grid) {
-      grid.destroy();
-    }
- });
+let columnDefs: (ColDef | ColGroupDef)[] = [
+    { headerName: 'Broker', field: 'broker', flex: 1 },
+    { headerName: 'Asset', field: 'asset', flex: 1 },
+    { headerName: 'Currency', field: 'ccy', flex: 1 },
+    {
+      headerName: 'Market Value', children: [
+        { headerName: 'CCY', field: 'marketValue', valueFormatter: currencyFormatter, type: 'numericColumn', flex: 2 },
+        { headerName: 'JPY', field: 'marketValueBaseCcy', valueFormatter: currencyFormatter, type: 'numericColumn', flex: 2 }
+      ]
+    },
+    {
+      headerName: 'Profit', children: [
+        { headerName: 'CCY', field: 'profit', valueFormatter: currencyFormatter, type: 'numericColumn', flex: 2 },
+        { headerName: 'JPY', field: 'profitBaseCcy', valueFormatter: currencyFormatter, type: 'numericColumn', flex: 2 }
+      ]
+    }    
+  ];
 
 </script>
 
-<div class="w-full h-full flex">
-<div bind:this={ref} style="height: 100%; width:100%"  class="ag-theme-alpine"></div>
-</div>
+<DataGrid {columnDefs} bind:data/>
--- electron-app/src/frontend/main.ts
+++ electron-app/src/frontend/main.ts
@@ -1,6 +1,7 @@
 import "svelte";
 import App from "./App.svelte";
-
+let ap: string = 'dd';
+console.log(ap);
 const app = new App({
   target: document.body,
   props: {
