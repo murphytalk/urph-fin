0f12f7eff0047d199857507db03b2939f43128b0
Mu Lu <murphytalk@gmail.com>
2022-08-21 17:35:38 +0900

    flutter app: call native lib

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -1,6 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:urph_fin/dylib_utils.dart';
+import 'dart:ffi';
+import 'dart:isolate';
+
+
+final dl = dlopenPlatformSpecific('urph-fin-core-dart', path:'/home/mu/work/urph-fin/cxx/build/urph-fin-core-dart/');
+void requestExecuteCallback(dynamic message) {
+  final int work_address = message;
+  final work = Pointer<Work>.fromAddress(work_address);
+  print("Dart:   Calling into C to execute callback ($work).");
+  executeCallback(work);
+  print("Dart:   Done with callback.");
+}
+
+final executeCallback = dl.lookupFunction<Void Function(Pointer<Work>),
+    void Function(Pointer<Work>)>('execute_callback');
+
+//urph-fin init
+final urphFinInit = dl.lookupFunction<Bool Function(), bool Function()>('dart_urph_fin_core_init');
+
+final register_init_callback = dl.lookupFunction<
+    Void Function(Int64 sendPort,
+        Pointer<NativeFunction<Void Function(Pointer<Void>)>> functionPointer),
+    void Function(int sendPort,
+        Pointer<NativeFunction<Void Function(Pointer<Void>)>> functionPointer)>(
+    'register_init_callback');
+
+void init_callback(Pointer<Void> p)
+{
+  print("urph-fin init done");
+}
+final initCallbackFP = Pointer.fromFunction<Void Function(Pointer<Void>)>(init_callback);
+
+class Work extends Opaque {}
+
+void setup_ffi()
+{
+  final initializeApi = dl.lookupFunction<IntPtr Function(Pointer<Void>),
+      int Function(Pointer<Void>)>("InitializeDartApiDL");
+  final r = initializeApi(NativeApi.initializeApiDLData);
+  print("initApi $r");
+
+  final interactiveCppRequests = ReceivePort()..listen(requestExecuteCallback);
+  final int nativePort = interactiveCppRequests.sendPort.nativePort;
+  register_init_callback(nativePort, initCallbackFP);
+  final initResult = urphFinInit();
+  print('urph-fin init result: $initResult');
+}
 
 void main() {
+  setup_ffi();
   runApp(const MyApp());
 }
 
