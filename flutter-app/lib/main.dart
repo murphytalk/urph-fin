53c6bba2598ebaee25ec54a185ad257e670d29f1
Mu Lu <murphytalk@gmail.com>
2022-08-28 23:30:05 +0900

    overview data table load data

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -4,17 +4,17 @@ import 'dart:ffi';
 import 'package:urph_fin/dao.dart';
 import 'package:window_manager/window_manager.dart';
 
-void onUrphFinInitDone(Pointer<Void> p)
-{
+void onUrphFinInitDone(Pointer<Void> p) {
   log("urph-fin init done");
 }
 
 const TITLE = 'UrphFin';
 
 void main() {
-  setupFFI(Pointer.fromFunction<Void Function(Pointer<Void>)>(onUrphFinInitDone));
+  setupFFI(
+      Pointer.fromFunction<Void Function(Pointer<Void>)>(onUrphFinInitDone));
   runApp(const MyApp());
-  windowManager.waitUntilReadyToShow().then((_) async{
+  windowManager.waitUntilReadyToShow().then((_) async {
     //await windowManager.setAsFrameless();
     await windowManager.setTitle(TITLE);
   });
@@ -27,50 +27,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          //title: const Text(TITLE),
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
         ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              WidthAwareView()
-            ],
+        home: Scaffold(
+          appBar: AppBar(
+              // Here we take the value from the MyHomePage object that was created by
+              // the App.build method, and use it to set our appbar title.
+              //title: const Text(TITLE),
+              ),
+          body: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[WidthAwareView()],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
 
@@ -102,55 +99,49 @@ class Overview extends StatefulWidget {
 }
 
 class _OverviewState extends State<Overview> {
+  List<DataRow> _rows = [
+    DataRow(
+      cells: <DataCell>[
+        DataCell(Text('Sarah')),
+        DataCell(Text('19')),
+        DataCell(Text('Student')),
+      ],
+    ),
+    DataRow(
+      cells: <DataCell>[
+        DataCell(Text('Janine')),
+        DataCell(Text('43')),
+        DataCell(Text('Professor')),
+      ],
+    ),
+    DataRow(cells: <DataCell>[
+      DataCell(Text('William')),
+      DataCell(Text('27')),
+      DataCell(Text('Associate Professor')),
+    ])
+  ];
+
   @override
   Widget build(BuildContext context) {
-    print('build overview');
-    return DataTable(
-      columns: const <DataColumn>[
-        DataColumn(
-          label: Text(
-            'Name',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
+    return DataTable(columns: const <DataColumn>[
+      DataColumn(
+        label: Text(
+          'Name',
+          style: TextStyle(fontStyle: FontStyle.italic),
         ),
-        DataColumn(
-          label: Text(
-            'Age',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
+      ),
+      DataColumn(
+        label: Text(
+          'Age',
+          style: TextStyle(fontStyle: FontStyle.italic),
         ),
-        DataColumn(
-          label: Text(
-            'Role',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
+      ),
+      DataColumn(
+        label: Text(
+          'Role',
+          style: TextStyle(fontStyle: FontStyle.italic),
         ),
-      ],
-      rows: const <DataRow>[
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Sarah')),
-            DataCell(Text('19')),
-            DataCell(Text('Student')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Janine')),
-            DataCell(Text('43')),
-            DataCell(Text('Professor')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('William')),
-            DataCell(Text('27')),
-            DataCell(Text('Associate Professor')),
-          ],
-        ),
-      ],
-    );
+      ),
+    ], rows: _rows);
   }
 }
-
-
