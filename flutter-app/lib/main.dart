a711a518c03e3c81f31805c115f9eb29a1d06dd1
Mu Lu <murphytalk@gmail.com>
2022-09-03 20:39:15 +0900

    removed the extra column layer under app body

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -16,8 +16,7 @@ void onUrphFinInitDone(Pointer<Void> p) {
 Future<Pointer<Void>> initUrphFin() {
   final c = Completer<Pointer<Void>>();
   _urphFinInitCompleter = c;
-  setupFFI(
-      Pointer.fromFunction<Void Function(Pointer<Void>)>(onUrphFinInitDone));
+  setupFFI(Pointer.fromFunction<Void Function(Pointer<Void>)>(onUrphFinInitDone));
   return c.future;
 }
 
@@ -43,47 +42,27 @@ class MyApp extends StatelessWidget {
           Widget child;
           if (snapshot.hasData) {
             child = MaterialApp(
-                theme: ThemeData(
-                  // This is the theme of your application.
-                  //
-                  // Try running your application with "flutter run". You'll see the
-                  // application has a blue toolbar. Then, without quitting the app, try
-                  // changing the primarySwatch below to Colors.green and then invoke
-                  // "hot reload" (press "r" in the console where you ran "flutter run",
-                  // or simply save your changes to "hot reload" in a Flutter IDE).
-                  // Notice that the counter didn't reset back to zero; the application
-                  // is not restarted.
-                  primarySwatch: Colors.blue,
-                ),
-                home: Scaffold(
-                  appBar: AppBar(
-                      // Here we take the value from the MyHomePage object that was created by
-                      // the App.build method, and use it to set our appbar title.
-                      //title: const Text(TITLE),
-                      ),
-                  body: Center(
-                    // Center is a layout widget. It takes a single child and positions it
-                    // in the middle of the parent.
-                    child: Column(
-                      // Column is also a layout widget. It takes a list of children and
-                      // arranges them vertically. By default, it sizes itself to fit its
-                      // children horizontally, and tries to be as tall as its parent.
-                      //
-                      // Invoke "debug painting" (press "p" in the console, choose the
-                      // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                      // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                      // to see the wireframe for each widget.
-                      //
-                      // Column has various properties to control how it sizes itself and
-                      // how it positions its children. Here we use mainAxisAlignment to
-                      // center the children vertically; the main axis here is the vertical
-                      // axis because Columns are vertical (the cross axis would be
-                      // horizontal).
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[WidthAwareView()],
+              theme: ThemeData(
+                // This is the theme of your application.
+                //
+                // Try running your application with "flutter run". You'll see the
+                // application has a blue toolbar. Then, without quitting the app, try
+                // changing the primarySwatch below to Colors.green and then invoke
+                // "hot reload" (press "r" in the console where you ran "flutter run",
+                // or simply save your changes to "hot reload" in a Flutter IDE).
+                // Notice that the counter didn't reset back to zero; the application
+                // is not restarted.
+                primarySwatch: Colors.blue,
+              ),
+              home: Scaffold(
+                appBar: AppBar(
+                    // Here we take the value from the MyHomePage object that was created by
+                    // the App.build method, and use it to set our appbar title.
+                    //title: const Text(TITLE),
                     ),
-                  ),
-                ));
+                body: const Center(child: WidthAwareView()),
+              ),
+            );
           } else {
             child = const AwaitWidget(caption: "Logging in");
           }
@@ -106,8 +85,6 @@ class _WidthAwareViewState extends State<WidthAwareView> {
     // https://api.flutter.dev/flutter/widgets/MediaQuery/of.html
     final screenWidth = MediaQuery.of(context).size.width;
     print('build split view, screen width = $screenWidth');
-    return const Expanded(
-      child: OverviewWidget(),
-    );
+    return const OverviewWidget();
   }
 }
