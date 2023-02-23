0b8380863dd7cfcb0436454dbd43093ea5873a4b
Mu Lu <murphytalk@gmail.com>
2023-02-23 19:50:47 +0900

    use AWS logging only when AWS is used

--- cxx/urph-fin-core/src/utils.hxx
+++ cxx/urph-fin-core/src/utils.hxx
@@ -1,18 +1,15 @@
 #ifndef UTILS_HXX_
 #define UTILS_HXX_
 
-#if defined(GTEST_INCLUDE_GTEST_GTEST_H_) || defined(__ANDROID__)
+#if defined(GTEST_INCLUDE_GTEST_GTEST_H_) || defined(__ANDROID__) || defined(DUMMY_LOG)
 
 #define LOG_INFO(tag, stream_expr)
 #define LOG_ERROR(tag, stream_expr)
 #define LOG_DEBUG(tag, stream_expr)
 
-#else
-
-//#include <aws/logs/LogMacros.h>
+#elif defined (AWS)
 
 #include <aws/core/utils/logging/LogMacros.h>
-
 #define LOG_INFO(tag, stream_expr) AWS_LOGSTREAM_INFO(tag, stream_expr)
 #define LOG_WARNING(tag, stream_expr) AWS_LOGSTREAM_WARN(tag, stream_expr)
 #define LOG_ERROR(tag, stream_expr) AWS_LOGSTREAM_ERROR(tag, stream_expr)
