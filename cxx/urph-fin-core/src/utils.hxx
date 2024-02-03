57adf8cbb021cf2ea74098eb5c78e4035ea9b3b1
Mu Lu <murphytalk@gmail.com>
2024-02-04 00:10:14 +0900

    log tag

--- cxx/urph-fin-core/src/utils.hxx
+++ cxx/urph-fin-core/src/utils.hxx
@@ -16,10 +16,10 @@
 #include <thread>
 #include <iostream>
 
-#define LINFO(tag, stream_expr)  LOG(INFO)  << "[" << std::hex << std::this_thread::get_id() << "] " << std::dec << stream_expr << "\n"
-#define LWARN(tag, stream_expr)  LOG(WARN)  << "[" << std::hex << std::this_thread::get_id() << "] " << std::dec << stream_expr << "\n"
-#define LERROR(tag, stream_expr) LOG(ERROR) << "[" << std::hex << std::this_thread::get_id() << "] " << std::dec << stream_expr << "\n"
-#define LDEBUG(tag, stream_expr) LOG(DEBUG) << "[" << std::hex << std::this_thread::get_id() << "] " << std::dec << stream_expr << "\n"
+#define LINFO(tag, stream_expr)  LOG(INFO)  << "[" << tag << "] " << "[" << std::hex << std::this_thread::get_id() << "] " << std::dec << stream_expr << "\n"
+#define LWARN(tag, stream_expr)  LOG(WARN)  << "[" << tag << "] " << "[" << std::hex << std::this_thread::get_id() << "] " << std::dec << stream_expr << "\n"
+#define LERROR(tag, stream_expr) LOG(ERROR) << "[" << tag << "] " << "[" << std::hex << std::this_thread::get_id() << "] " << std::dec << stream_expr << "\n"
+#define LDEBUG(tag, stream_expr) LOG(DEBUG) << "[" << tag << "] " << "[" << std::hex << std::this_thread::get_id() << "] " << std::dec << stream_expr << "\n"
 
 #else
 
